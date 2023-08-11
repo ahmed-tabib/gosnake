4dad3dfc3ad8e01b18ad39efb66a0e44f059cebe
ahmed-tabib <ahmed.tabib0@gmail.com>
2023-08-11 18:44:44 +0100

    added FastRemove function

--- internal/cachesnake/util.go
+++ internal/cachesnake/util.go
@@ -4,6 +4,14 @@ import (
 	"crypto/rand"
 )
 
+// removes an element from a slice quickly. Changes the order of elements
+func FastRemove[T any](s []T, i int) []T {
+	var default_val T
+	s[len(s)-1], s[i] = default_val, s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+// generate a random alphabetical lowercase string
 func GenRandString(length int) string {
 	str_bytes := make([]byte, length)
 	rand.Read(str_bytes)
